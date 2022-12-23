e01aa1ee874b603b80b777c03c12e3098fa379fd
Lehcar <tweetyho1234@gmail.com>
2022-12-23 16:06:55 +0800

    2022.12.23

---
+++ src/component/photos.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { useEffect, useRef, useState } from 'react';
+
+export type Props = {
+
+};
+
+const Photos = ({  }: Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default Photos;
+
--- src/component/wall.tsx
+++ src/component/wall.tsx
@@ -1,20 +1,20 @@
-import React from "react"
-import { useEffect, useRef, useState } from 'react';
+import React from "react";
+import { useEffect, useRef, useState } from "react";
+import Photos from "./photos";
 
-export type Props = {
+export type Props = {};
 
-};
-
-const name = ({  }: Props) => {
+const Wall = ({}: Props) => {
   return (
     <div className="w-full h-full bg-zinc-200">
-      
+        
+      <div className="w-full h-30">
+        <h1 className="text-slate-600 ">Photo Wall</h1>
+      </div>
+
+      <Photos></Photos>
     </div>
-  )
+  );
 };
 
-export default name;
-
-
-
-
+export default Wall;
